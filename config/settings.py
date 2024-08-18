125724041eba33d9178d144f0e8085ebeda5c882
SwthSawTheme <sawtheme1@gmail.com>
2024-08-18 17:56:59 -0300

    Pacote de classes de Settings criado

---
+++ config/settings.py
@@ -0,0 +1,8 @@
+# Classe para configurações adicionais
+
+class Settings(object):
+    def __init__(self,configuracoes:dict):
+        self.configuracoes = {}
+
+    def carregar_configuracoes():
+        pass
